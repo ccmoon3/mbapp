664d73dc43ef53e16ec1ed53c7f0d8d2b07382c5
XY Chen <mail-999@qq.com>
2016-03-17 18:51:32 -0400

    add takePhoto()

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -1,6 +1,46 @@
 angular.module('starter.controllers', [])
 
-.controller('DashCtrl', function($scope) {})
+.config(function($compileProvider){
+$compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|file|blob|cdvfile|content):|data:image\//);
+})
+
+.controller('DashCtrl', function($scope, $cordovaCamera) {
+ /*  $scope.takePhoto = function() {
+         console.log("takePhoto");
+         Camera.getPicture().then(function(imageURI) {
+              console.log(imageURI);
+              $scope.lastPhoto = imageURI;
+              $scope.ImageURI =  imageURI;
+       //       UploadPicture(imageURI);
+         }, function(err) {
+            console.err(err);
+         }, {
+             quality: 75,
+             allowEdit : true,
+             saveToPhotoAlbum: true
+         });
+    }*/
+    $scope.takePhoto = function () {
+
+                           var options = {
+                             quality: 50,
+                             allowEdit: true,
+                             targetWidth: 100,
+                             targetHeight: 100,
+                             saveToPhotoAlbum: true,
+                       	  correctOrientation:true
+                           };
+
+                           $cordovaCamera.getPicture(options).then(function(imageData) {
+                             var image = document.getElementById('myImage');
+                             image.src = "data:image/jpeg;base64," + imageData;
+                             console.log(imageData);
+                           }, function(err) {
+                             // error
+                           });
+
+                         }
+})
 
 .controller('ChatsCtrl', function($scope, Chats) {
   // With the new view caching in Ionic, Controllers are only called
