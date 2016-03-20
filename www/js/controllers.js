7932804b675d4962ad4228fe44320a5a41c5f4a8
XY Chen <mail-999@qq.com>
2016-03-20 13:33:11 -0400

    web ok

--- www/js/controllers.js
+++ www/js/controllers.js
@@ -5,41 +5,194 @@ $compileProvider.imgSrcSanitizationWhitelist(/^\s*(https?|file|blob|cdvfile|cont
 })
 
 .controller('DashCtrl', function($scope, $cordovaCamera) {
- /*  $scope.takePhoto = function() {
-         console.log("takePhoto");
-         Camera.getPicture().then(function(imageURI) {
-              console.log(imageURI);
-              $scope.lastPhoto = imageURI;
-              $scope.ImageURI =  imageURI;
-       //       UploadPicture(imageURI);
-         }, function(err) {
-            console.err(err);
-         }, {
-             quality: 75,
-             allowEdit : true,
-             saveToPhotoAlbum: true
-         });
-    }*/
+    var MaxWidth = 0.9*window.innerWidth;
+    var MaxHeight = 0.8*window.innerHeight;
     $scope.takePhoto = function () {
 
-                           var options = {
+                /*           var options = {
                              quality: 50,
+                             destinationType: Camera.DestinationType.FILE_URI,
+                             sourceType: Camera.PictureSourceType.CAMERA,
                              allowEdit: true,
-                             targetWidth: 100,
-                             targetHeight: 100,
                              saveToPhotoAlbum: true,
-                       	  correctOrientation:true
+                       	     correctOrientation:true
                            };
 
-                           $cordovaCamera.getPicture(options).then(function(imageData) {
-                             var image = document.getElementById('myImage');
-                             image.src = "data:image/jpeg;base64," + imageData;
-                             console.log(imageData);
-                           }, function(err) {
+                           $cordovaCamera.getPicture(options).then(function(imageURI) {*/
+                             imageURI = "img/test.png";
+                             loadImage(imageURI, function(canvas){
+                                dataURL = canvas.toDataURL("image/png");
+                       //         console.log(dataURL);
+                         //       alert(dataURL);
+                                $(function() {
+                                                                           var params = {
+                                                                               // Request parameters
+                                                                               "returnFaceId": "false",
+                                                                               "returnFaceLandmarks": "false",
+                                                                               "returnFaceAttributes": "gender",
+                                                                           };
+
+                                                                           $.ajax({
+                                                                               url: "https://api.projectoxford.ai/face/v1.0/detect?" + $.param(params),
+                                                                     //          url: "https://api.projectoxford.ai/face/v1.0/detect",
+                                                                               beforeSend: function(xhrObj){
+                                                                                   // Request headers
+                                                                          //         xhrObj.setRequestHeader("Content-Type","application/json");
+                                                                                   xhrObj.setRequestHeader("Content-Type", "application/octet-stream");
+                                                                                   xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","305d3ac2f26f4247bf9175c59f5802ab");
+                                                                               },
+                                                                               type: "POST",
+                                                                               // Request body
+                                                                               data: makeblob(dataURL),
+                                                                      //         data:'{"url":"http://images.wisegeek.com/triangular-face.jpg"}',
+                                                                               processData: false,
+                                                                           })
+                                                                           .done(function(data) {
+                                                                               console.log(JSON.stringify(data));
+                                                                               alert("face++ send done.");
+                                                                               alert(data[0].faceAttributes.gender);
+                                                                               console.log(data[0].faceAttributes.gender);
+                                                                           })
+                                                                           .fail(function(error) {
+                                                                               console.log(JSON.stringify(error));
+                                                                               alert("face++ error.");
+                                                                           });
+                                                                       });
+                          /*        $(function() {
+                                         var params = {
+                                             // Request parameters
+                                             "visualFeatures": "Face",
+                                         };
+
+                                         $.ajax({
+                                              url: "https://api.projectoxford.ai/vision/v1/analyses?" + $.param(params),
+                                             beforeSend: function(xhrObj){
+                                                 // Request headers
+                                                 xhrObj.setRequestHeader("Content-Type","application/octet-stream");
+                                                 xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","721e1c0487cb45d8b6356644c5c8709e");
+                                             },
+                                             type: "POST",
+                                             // Request body
+                                             data: makeblob(dataURL),
+                                             processData: false,
+                                         })
+                                         .done(function(data) {
+                                             console.log(JSON.stringify(data));
+                                             alert("success");
+                                         })
+                                         .fail(function(error) {
+                                             console.log(JSON.stringify(error));
+                                             alert("error");
+                                         });
+                                     });*/
+                                });
+
+                     /*      }, function(err) {
                              // error
-                           });
+                             alert(err);
+                           });*/
 
                          }
+
+         function loadImage(imageURI, callback){
+                var canvas = document.getElementById('selfie');
+                var ctx = canvas.getContext('2d');
+                ctx.clearRect(0, 0, canvas.width, canvas.height);
+                var image = new Image();
+
+               image.onload = function() {
+                   	//todo:wrap function
+                    if(image.width> MaxWidth ||image.height> MaxHeight||(image.height< MaxHeight && image.width< MaxWidth)){
+                            scale =  Math.min(MaxWidth / image.width,MaxHeight / image.height );
+                            image.height = scale * image.height;
+                            image.width = scale * image.width;
+                    }
+                    canvas.width = image.width;
+                    canvas.height = image.height;
+                    Width = canvas.width;
+                    Height = canvas.height;
+                    ctx.drawImage(image, 0, 0, image.width, image.height);
+                    callback(canvas);
+               };
+
+               image.src = imageURI;
+         }
+
+
+
+      // convert base64/URLEncoded data component to raw binary data held in a string
+         makeblob = function (dataURL) {
+                     var BASE64_MARKER = ';base64,';
+                     if (dataURL.indexOf(BASE64_MARKER) == -1) {
+                         var parts = dataURL.split(',');
+                         var contentType = parts[0].split(':')[1];
+                         var raw = decodeURIComponent(parts[1]);
+                         return new Blob([raw], { type: contentType });
+                     }
+                     var parts = dataURL.split(BASE64_MARKER);
+                     var contentType = parts[0].split(':')[1];
+                     var raw = window.atob(parts[1]);
+                     var rawLength = raw.length;
+
+                     var uInt8Array = new Uint8Array(rawLength);
+
+                     for (var i = 0; i < rawLength; ++i) {
+                         uInt8Array[i] = raw.charCodeAt(i);
+                     }
+
+                     return new Blob([uInt8Array], { type: contentType });
+
+          /*           BlobToBinaryString(new Blob([uInt8Array], { type: contentType }), function(databinary){
+                        console.dir(databinary);
+                        $(function() {
+                                                                       var params = {
+                                                                           // Request parameters
+                                                                           "returnFaceId": "true",
+                                                                           "returnFaceLandmarks": "false",
+                                                                           "returnFaceAttributes": "gender",
+                                                                       };
+
+                                                                       $.ajax({
+                                                                           url: "https://api.projectoxford.ai/face/v1.0/detect?" + $.param(params),
+                                                                           beforeSend: function(xhrObj){
+                                                                               // Request headers
+                                                                               xhrObj.setRequestHeader("Content-Type","application/json");
+                                                                               xhrObj.setRequestHeader("Ocp-Apim-Subscription-Key","305d3ac2f26f4247bf9175c59f5802ab");
+                                                                           },
+                                                                           type: "POST",
+                                                                           // Request body
+                                                                           data: JSON.stringify(makeblob(dataURL)),
+                                                                       })
+                                                                       .done(function(data) {
+                                                                           alert("done.");
+                                                                           alert(data);
+                                                                           alert("Gender:"+data.faceAttributes.gender);
+                                                                       })
+                                                                       .fail(function(error) {
+                                                                           alert("fail.");
+                                                                           console.log(JSON.stringify(error));
+                                                                           alert(error);
+                                                                           console.log(error.message);
+                                                                           alert("Error Message:"+error.message);
+                                                                       });
+                                                                   });
+                     });*/
+                 }
+
+        BlobToBinaryString = function(blob, callback, opt_errorCallback) {
+                     var reader = new FileReader();
+                     reader.onload = function(e) {
+                       alert(e.target.result);
+                       callback(e.target.result);
+                     };
+                     reader.onerror = function(e) {
+                       if (opt_errorCallback) {
+                         opt_errorCallback(e);
+                       }
+                     };
+                     reader.readAsBinaryString(blob);
+                 }
+
 })
 
 .controller('ChatsCtrl', function($scope, Chats) {
